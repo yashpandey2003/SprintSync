167ffd836295357e6702666300683569718b6352
yashpandey2003 <yashisro@gmail.com>
2025-02-24 22:15:42 +0530

    project api completed

--- project-management/src/main/java/com/yash/project_management/controller/ProjectController.java
+++ project-management/src/main/java/com/yash/project_management/controller/ProjectController.java
@@ -1,10 +1,11 @@
 package com.yash.project_management.controller;
 
+import com.yash.project_management.model.Chat;
 import com.yash.project_management.model.Project;
 import com.yash.project_management.model.User;
+import com.yash.project_management.reponse.MessageResponse;
 import com.yash.project_management.service.ProjectService;
 import com.yash.project_management.service.UserService;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -27,10 +28,39 @@ public class ProjectController {
         return new ResponseEntity<>(projects, HttpStatus.OK);
     }
 
-    @GetMapping("/{projectId")
-    public ResponseEntity<Project> getProjectById(@PathVariable Long projectId, @RequestHeader("Authorization") String jwt) throws Exception {
+    @GetMapping("/{projectId}")
+    public ResponseEntity<Project> getProjectById(@RequestHeader("Authorization") String jwt, @RequestBody Project project) throws Exception {
         User user = userService.findUserProfileByJwt(jwt);
-        Project project = projectService.getProjectById(projectId);
-        return new ResponseEntity<>(project, HttpStatus.OK);
+        Project createdProject = projectService.createProject(project, user);
+        return new ResponseEntity<>(createdProject, HttpStatus.CREATED);
     }
+    @PatchMapping("/{projectId}")
+    public ResponseEntity<Project> updateProject(@PathVariable Long projectId, @RequestHeader("Authorization") String jwt, @RequestBody Project project) throws Exception{
+        User user = userService.findUserProfileByJwt(jwt);
+        Project updatedProject = projectService.updateProject(project, projectId);
+        return new ResponseEntity<>(updatedProject, HttpStatus.OK);
+    }
+    @DeleteMapping("/{projectId}")
+    public ResponseEntity<MessageResponse> deleteProject(@PathVariable Long projectId, @RequestHeader("Authorization") String jwt) throws Exception{
+        User user = userService.findUserProfileByJwt(jwt);
+        projectService.deleteProject(projectId, user.getId());
+        MessageResponse res = new MessageResponse("project deleted successfully");
+        return new ResponseEntity<>(res, HttpStatus.OK);
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<Project>> searchProject(@RequestParam(required = false) String keyword, @RequestHeader("Authorization") String jwt) throws Exception{
+        User user = userService.findUserProfileByJwt(jwt);
+        List<Project> projects = projectService.searchProjects(keyword, user);
+        return new ResponseEntity<>(projects, HttpStatus.OK);
+    }
+    @GetMapping("/{projectId}/chat")
+    public ResponseEntity<Chat> getChatByProjectId(@PathVariable Long projectId, @RequestHeader("Authorization") String jwt) throws Exception{
+        User user = userService.findUserProfileByJwt(jwt);
+        Chat chat = projectService.getChatByProjectId(projectId);
+        return new ResponseEntity<>(chat, HttpStatus.OK);
+    }
+
+
+
 }
---
+++ project-management/src/main/java/com/yash/project_management/reponse/MessageResponse.java
@@ -0,0 +1,12 @@
+package com.yash.project_management.reponse;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageResponse {
+    private String message;
+}
